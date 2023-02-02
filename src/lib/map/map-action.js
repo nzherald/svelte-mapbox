976aca019b95065998394ce8ac0c6f091558a090
Chris Knox <chris.knox@nzme.co.nz>
2023-02-03 10:08:27 +1300

    enable the provision of the mapbox object to the library rather than fetching it

--- src/lib/map/map-action.js
+++ src/lib/map/map-action.js
@@ -1,62 +1,81 @@
 import { load } from '../asset-loader.js'
 import { bindEvents } from '../event-bindings.js'
 
-export default function action (node, options = {}) {
+export default function action(node, options = {}) {
   let map
-
-  const resources = [
-    { type: 'script', attr: 'src', value: `//api.mapbox.com/mapbox-gl-js/${options.version}/mapbox-gl.js`, id: 'byk-gl-js' },
-    { type: 'link', attr: 'href', value: `//api.mapbox.com/mapbox-gl-js/${options.version}/mapbox-gl.css`, id: 'byk-gl-css' }
-  ]
-
-  const customStylesheetUrl = options.customStylesheetUrl
-  if (customStylesheetUrl) {
-    resources.push({ type: 'link', attr: 'href', value: customStylesheetUrl, id: 'byk-mcsu-css' })
-  }
-
   let unbind = () => {}
-  load(resources, () => {
-    unbind = init({ ...options, container: node }, node)
-  })
 
+  if (options.mapbox) {
+    mapbox = options.mapbox
+  } else {
+    const resources = [
+      {
+        type: 'script',
+        attr: 'src',
+        value: `//api.mapbox.com/mapbox-gl-js/${options.version}/mapbox-gl.js`,
+        id: 'byk-gl-js',
+      },
+      {
+        type: 'link',
+        attr: 'href',
+        value: `//api.mapbox.com/mapbox-gl-js/${options.version}/mapbox-gl.css`,
+        id: 'byk-gl-css',
+      },
+    ]
+
+    const customStylesheetUrl = options.customStylesheetUrl
+    if (customStylesheetUrl) {
+      resources.push({
+        type: 'link',
+        attr: 'href',
+        value: customStylesheetUrl,
+        id: 'byk-mcsu-css',
+      })
+    }
+
+    load(resources, () => {
+      unbind = init({ ...options, container: node }, node)
+    })
+  }
   return {
-    destroy () {
+    destroy() {
       unbind()
       map && map.remove && map.remove()
-    }
+    },
   }
 }
 
-function init (options, node) {
-  window.mapboxgl.accessToken = options.accessToken
-  const el = new window.mapboxgl.Map(options)
+function init(options, node) {
+  const mapbox = options.mapbox || window.mapboxgl
+  mapbox.accessToken = options.accessToken
+  const el = new mapbox.Map(options)
 
-  return bindEvents(el, handlers, window.mapboxgl, node)
+  return bindEvents(el, handlers, mapbox, node)
 }
 
 const handlers = {
-  dragend: el => {
-    return [ 'dragend', { center: el.getCenter() } ]
+  dragend: (el) => {
+    return ['dragend', { center: el.getCenter() }]
   },
-  drag: el => {
-    return [ 'drag', { center: el.getCenter() } ]
+  drag: (el) => {
+    return ['drag', { center: el.getCenter() }]
   },
-  moveend: el => {
-    return [ 'recentre', { center: el.getCenter() } ]
+  moveend: (el) => {
+    return ['recentre', { center: el.getCenter() }]
   },
   click: (el, { lngLat }) => {
-    return [ 'click', { lng: lngLat.lng, lat: lngLat.lat } ]
+    return ['click', { lng: lngLat.lng, lat: lngLat.lat }]
   },
-  zoomstart: el => {
-    return [ 'zoomstart', { zoom: el.getZoom() } ]
+  zoomstart: (el) => {
+    return ['zoomstart', { zoom: el.getZoom() }]
   },
-  zoom: el => {
-    return [ 'zoom', { zoom: el.getZoom() } ]
+  zoom: (el) => {
+    return ['zoom', { zoom: el.getZoom() }]
   },
-  zoomend: el => {
-    return [ 'zoomend', { zoom: el.getZoom() } ]
+  zoomend: (el) => {
+    return ['zoomend', { zoom: el.getZoom() }]
   },
   load: (el, ev, mapbox) => {
-    return [ 'ready', { map: el, mapbox } ]
-  }
+    return ['ready', { map: el, mapbox }]
+  },
 }
